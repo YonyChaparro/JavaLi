0a59ca6a7defdc3ebb0122fdf52fa43bf91f63cb
cristoferOrdonez <crordonezo@unal.edu.co>
2025-07-08 00:31:10 -0500

    Eliminación del enrutamiento.

--- Proyecto/PosYa-app/src/App.jsx
+++ Proyecto/PosYa-app/src/App.jsx
@@ -1,12 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeMenu from './pages/HomeMenu';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeMenu />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <>
+      <HomeMenu />
+    </>
+    );
 }
